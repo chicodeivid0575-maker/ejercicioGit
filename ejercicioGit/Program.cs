bd70fb1bae530e76c892ba97396829d56b4962b2
David Moreta <chicodeivid0575@gmail.com>
2026-01-06 15:30:48 -0400

    chore: restaurar Program.cs

--- ejercicioGit/Program.cs
+++ ejercicioGit/Program.cs
@@ -26,12 +26,5 @@ namespace ejercicioGit
 
             return resultado;
         }
-
-        static double Resta(double num1, double num2)
-        {
-            double resultado = num1 - num2;
-
-            return resultado;
-        }
     }
 }
